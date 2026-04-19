f24ce973eedc63ae2c1e3d5f2628299871967951
alejandrxmxnt <amont9483@gmail.com>
2026-04-19 04:50:21 -0400

    Creacion de compones categorias - no presento errores

--- src/app/grupo-componente/grupo-componente.ts
+++ src/app/grupo-componente/grupo-componente.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Categoria } from '../modelos/categoria.model';
+import { CategoriaDbservice } from '../servicios/categoria-dbservice';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-grupo-componente',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './grupo-componente.html',
   styleUrl: './grupo-componente.css',
 })
-export class GrupoComponente {}
+export class GrupoComponente implements OnInit{
+  categorias:Categoria[] = [];
+  nuevaCategoria: Categoria = {
+    tipo: "",
+    descripcion: ""
+  }
+
+  constructor (private categoriaDb: CategoriaDbservice){}
+
+  async ngOnInit() {
+    await this.cargarCategorias();
+  }
+
+  async cargarCategorias(){
+    this.categorias = await this.categoriaDb.todasCategorias();
+  }
+
+  async guardar(){
+    if(!this.nuevaCategoria.tipo){
+      alert('El tipo de categoria es obligatorio.');
+      return;
+    }
+    await this.categoriaDb.agregarCategoria(this.nuevaCategoria);
+    await this.cargarCategorias();
+
+    //limpieza de parametros
+    this.nuevaCategoria = {
+      tipo: "",
+      descripcion: ""
+    };
+ 
+  }
+
+  async eliminar (id: number) {
+    await this.categoriaDb.eliminarCategoria(id);
+    await this.cargarCategorias();
+  }
+}
